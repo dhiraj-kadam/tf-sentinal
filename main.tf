8510ef81177fc9b8a57409c22e58422fd2653119
Dhiraj Kadam <dkadam.freelance@gmail.com>
2021-10-19 21:54:14 +0530

    added policy

--- main.tf
+++ main.tf
@@ -24,26 +24,6 @@ resource "aws_kms_key" "a" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket_name}"
   
-  
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.a.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
-  tags = {
-    owner = "sentinel"
-  }
-}
-
-resource "aws_s3_bucket_policy" "b" {
-  bucket = aws_s3_bucket.bucket.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression's result to valid JSON syntax.
   policy = jsonencode({
     Version = "2012-10-17"
     Id      = "MYBUCKETPOLICY"
@@ -62,4 +42,42 @@ resource "aws_s3_bucket_policy" "b" {
       },
     ]
   })
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.a.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    owner = "sentinel"
+  }
 }
+
+# resource "aws_s3_bucket_policy" "b" {
+#   bucket = aws_s3_bucket.bucket.id
+
+#   # Terraform's "jsonencode" function converts a
+#   # Terraform expression's result to valid JSON syntax.
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Id      = "MYBUCKETPOLICY"
+#     Statement = [
+#       {
+#         Sid       = "enforce-https"
+#         Effect    = "Deny"
+#         Principal = "*"
+#         Action    = "s3:Put*"
+#         Resource = "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+#         Condition = {
+#           StringNotEquals = {
+#             "s3:x-amz-server-side-encryption-aws-kms-key-id" = "${aws_kms_key.a.arn}"
+#           }
+#         }
+#       },
+#     ]
+#   })
+# }
