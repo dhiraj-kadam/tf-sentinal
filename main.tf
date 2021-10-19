ea10bc57707b3d7ef36ff7ad08ed011de2ebf811
Dhiraj Kadam <dkadam.freelance@gmail.com>
2021-10-19 20:38:43 +0530

    added files

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 0.11.7"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+   description = "Name of the bucket to create"
+   default = "decay-test-s3-sentinel-bucket"
+}
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket_name}"
+  
+  
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         kms_master_key_id = "arn:aws:kms:us-east-1:753646501470:key/00c892e8-40c4-4048-a650-0f755876503d"
+#         sse_algorithm     = "aws:kms"
+#       }
+#     }
+#   }
+}
